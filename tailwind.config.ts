36255f3023b4c84bc53a8b280300aa699f8f1f2a
Svetlana Kostina <kostina@sibext.com>
2024-09-16 16:36:56 +0300

    [added] welcome page, fonts, colors variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "bright-gold": "var(--bright-gold)",
+        "muted-gold": "var(--muted-gold)",
+        "dark-red": "var(--dark-red)",
+        background: "var(----background)",
+        shadow: "var(--shadow)"
       },
     },
   },

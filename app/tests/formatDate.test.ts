b9859f9bacbee8b90d34b2c2962cdb6b3703338c
Svetlana Kostina <kostina@sibext.com>
2024-09-24 14:15:26 +0300

    [fixed] test cleanup

--- app/tests/formatDate.test.ts
+++ app/tests/formatDate.test.ts
@@ -1,20 +1,9 @@
 import formatDate from '@/app/helpers/formatDate';
-import { advanceTo, clear } from 'jest-date-mock';
-
-beforeAll(() => {
-  advanceTo(new Date(2018, 5, 27, 0, 0, 0));
-});
-
-afterAll(() => {
-  clear();
-});
 
 describe('formatDate', ()=>{
   test('only date', ()=>{
     const dateFromDb = '2024-09-22T21:00:00.000Z';
     const resultString = '09/22/2024 21:00';
-    const offset = new Date().getTimezoneOffset();
-    console.log(offset, 'new Date().getTimezoneOffset()'); //вывел -180
     expect(formatDate(dateFromDb)).toEqual(resultString);
   });
   test('date + time', ()=>{
